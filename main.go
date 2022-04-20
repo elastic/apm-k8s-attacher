3684633fa2befb9e8f2c638a92f4ee39a05d2bea
stuart nelson <stuartnelson3@gmail.com>
2022-04-20 16:39:13 +0200

    remove more dead code

--- main.go
+++ main.go
@@ -64,22 +64,18 @@ type agentConfig struct {
 const apmAnnotation = "elastic-apm-agent"
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.l.Println("request received")
-	// read the body / request
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		sendError(err, w)
 		return
 	}
 
-	// mutate the request
-	mutated, err := mutate(body, true)
+	mutated, err := mutate(body)
 	if err != nil {
 		sendError(err, w)
 		return
 	}
 
-	// and write it back
 	w.WriteHeader(http.StatusOK)
 	w.Write(mutated)
 }
@@ -90,8 +86,7 @@ func sendError(err error, w http.ResponseWriter) {
 	fmt.Fprintf(w, "%s", err)
 }
 
-func mutate(body []byte, verbose bool) ([]byte, error) {
-	// unmarshal request into AdmissionReview struct
+func mutate(body []byte) ([]byte, error) {
 	admReview := admissionv1.AdmissionReview{}
 	if err := json.Unmarshal(body, &admReview); err != nil {
 		return nil, fmt.Errorf("unmarshaling request failed with %s", err)
@@ -105,16 +100,13 @@ func mutate(body []byte, verbose bool) ([]byte, error) {
 	resp := admissionv1.AdmissionResponse{}
 
 	if ar != nil {
-
-		// get the Pod object and unmarshal it into its struct, if we cannot, we might as well stop here
 		if err := json.Unmarshal(ar.Object.Raw, &pod); err != nil {
 			return nil, fmt.Errorf("unable unmarshal pod json object %v", err)
 		}
-		// set response options
 		resp.Allowed = true
 		resp.UID = ar.UID
 		pT := admissionv1.PatchTypeJSONPatch
-		resp.PatchType = &pT // it's annoying that this needs to be a pointer as you cannot give a pointer to a constant?
+		resp.PatchType = &pT
 
 		spec := pod.Spec
 
@@ -134,25 +126,16 @@ func mutate(body []byte, verbose bool) ([]byte, error) {
 			patches = append(patches, createEnvVariablesPatches(container.Env == nil, index)...)
 		}
 
-		// parse the []map into JSON
 		resp.Patch, err = json.Marshal(patches)
-
-		// Success, of course ;)
 		resp.Result = &metav1.Status{
 			Status: "Success",
 		}
 
 		admReview.Response = &resp
-		// back into JSON so we can return the finished AdmissionReview w/ Response directly
-		// w/o needing to convert things in the http handler
 		responseBody, err = json.Marshal(admReview)
 		if err != nil {
-			return nil, err // untested section
+			return nil, err
 		}
-	}
-
-	if verbose {
-		log.Printf("resp: %s\n", string(responseBody)) // untested section
 	}
 
 	return responseBody, nil
