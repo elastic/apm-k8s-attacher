1412f91fa49c2b21513e8ab6c7b23171226411c2
stuart nelson <stuartnelson3@gmail.com>
2022-04-21 15:09:51 +0200

    rename script

--- update-webhook.sh
+++ update-webhook.sh
@@ -6,9 +6,10 @@ namespace=elastic-apm
 
 go build .
 make .webhook
-# docker push stuartnelson3/webhook
+docker push stuartnelson3/webhook
 webhook_name=$(kubectl get -o name pods --namespace=$namespace)
 kubectl delete $webhook_name --namespace=$namespace
-sleep 3
+sleep 1
 webhook_name=$(kubectl get -o name pods --namespace=$namespace)
+kubectl wait --for=condition=Ready=true $webhook_name --namespace=$namespace
 kubectl logs -f $webhook_name --namespace=$namespace
