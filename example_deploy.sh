3b7255b4699cfacc4284434d8dafff93e9692c8d
stuart nelson <stuartnelson3@gmail.com>
2022-04-21 10:42:20 +0200

    example deploy w and w/o annotation

---
+++ example_deploy.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -exuo errexit
+
+export APP="example-app"
+export NAMESPACE="${2:-default}"
+
+revision=$(date +%s)
+
+kubectl apply -f - <<EOF
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${APP}
+  namespace: ${NAMESPACE}
+  labels:
+    app: ${APP}
+  annotations:
+    deployment.kubernetes.io/revision: "$revision"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${APP}
+  template:
+    metadata:
+      name: ${APP}
+      labels:
+        app: ${APP}
+      annotations:
+        deployment.kubernetes.io/revision: "$revision"
+    spec:
+      containers:
+        - name: example-app
+          image: hashicorp/http-echo:alpine
+          imagePullPolicy: Always
+          args:
+          - "-text='hello world'"
+          ports:
+          - containerPort: 5678
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${APP}-annotation
+  namespace: ${NAMESPACE}
+  labels:
+    app: ${APP}-annotation
+  annotations:
+    deployment.kubernetes.io/revision: "$revision"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${APP}-annotation
+  template:
+    metadata:
+      name: ${APP}-annotation
+      labels:
+        app: ${APP}-annotation
+      annotations:
+        deployment.kubernetes.io/revision: "$revision"
+        elastic-apm-agent: java
+    spec:
+      containers:
+        - name: example-app
+          image: hashicorp/http-echo:alpine
+          imagePullPolicy: Always
+          args:
+          - "-text='hello world'"
+          ports:
+          - containerPort: 5678
+EOF
