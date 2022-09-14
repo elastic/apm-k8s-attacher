0b14c23bf4492ef16bc47564c2e881b3b4178f93
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-09-14 10:47:46 +0100

    docker: enable short git tag

--- .ci/docker-package-push.sh
+++ .ci/docker-package-push.sh
@@ -7,15 +7,19 @@
 # - REPO: the docker repo name
 # - NAME: the docker image name
 # - TAG: the docker tag version
+# - SHORT_GIT_SHA: whether to short the git sha, aka the given TAG argument.
 
 set -euo pipefail
 
 export REPO=${1:?docker repo not set}
 export NAME=${2:?docker image name not set}
 export TAG=${3:?docker tag not set}
+export SHORT_GIT_SHA=${4:-false}
 
 # Use the short git SHA
-TAG=${TAG:0:7}
+if [ "$SHORT_GIT_SHA" == "true" ] ; then
+	TAG=${TAG:0:7}
+fi
 
 fqn="${REPO}/${NAME}:${TAG}"
 latest="${REPO}/${NAME}:latest"
