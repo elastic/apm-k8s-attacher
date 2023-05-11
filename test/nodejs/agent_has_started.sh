0554b54ec4a43dc6514411daf6cc102b8da605cb
Jack Shirazi <jack.shirazi@elastic.co>
2023-05-11 13:29:46 +0100

    trigger integration testing on pushes and PRs

--- test/nodejs/agent_has_started.sh
+++ test/nodejs/agent_has_started.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#initial 10 second delay for restricted CPUs making agent slow starting 
+sleep 10
 START_MESSAGE=`kubectl logs nodejs-test-app | grep 'agent\\\":{\\\"name\\\":\\\"nodejs\\\",'`
 if [ "x$START_MESSAGE" = "x" ]
 then
