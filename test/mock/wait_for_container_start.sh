5de76936b5159c502cc09f3d06fefadda57ce2d9
Trent Mick <trent.mick@elastic.co>
2024-01-12 15:54:35 -0800

    fix a path issue; remove debug output

--- test/mock/wait_for_container_start.sh
+++ test/mock/wait_for_container_start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#XXX
-set -x
-
 MAX_WAIT_SECONDS=60
 POD_NAME=$1
 
@@ -11,7 +8,6 @@ count=0
 while [ $count -lt $MAX_WAIT_SECONDS ]
 do
   count=`expr $count + 1`
-  kubectl get pod -A #XXX
   STARTED=`kubectl get pod -A | grep $POD_NAME | grep 'Running'`
   if [ "$STARTED" != "" ]
   then
@@ -23,8 +19,5 @@ done
 echo "error: pod matching '$POD_NAME' failed to start within $MAX_WAIT_SECONDS seconds"
 echo "-- pod info:"
 kubectl get pod -A
-echo "-- pod logs:"
-kubectl logs "$POD_NAME"
-echo "-- docker images:"
-docker images
+echo "--"
 exit 1
