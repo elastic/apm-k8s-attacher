d8ac3cf7b9f51c0711ed3fc7452d6bf79708ba39
Martijn Laarman <Mpdreamz@gmail.com>
2024-07-09 16:42:40 +0200

    more info from agent_has_started.sh

--- test/dotnet/agent_has_started.sh
+++ test/dotnet/agent_has_started.sh
@@ -4,7 +4,9 @@ sleep 10
 START_MESSAGE=`kubectl logs dotnet-test-app | grep '- Elastic APM .NET Agent, version:'`
 if [ "x$START_MESSAGE" = "x" ]
 then
+  echo "APM Agent appears to have not been started"
   exit 1
 else
+  echo "Found APM agent in the kubctl logs"
   exit 0
 fi
