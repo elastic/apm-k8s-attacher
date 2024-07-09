26c66a7709ab9ac6336f69f682ac181e3e160ec1
Martijn Laarman <Mpdreamz@gmail.com>
2024-07-09 16:29:59 +0200

    :grep => grep, trying out fleet but its VI bindings are still crap :)

--- test/dotnet/agent_has_started.sh
+++ test/dotnet/agent_has_started.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #initial 10 second delay for restricted CPUs making agent slow starting 
 sleep 10
-START_MESSAGE=`kubectl logs dotnet-test-app | :grep '- Elastic APM .NET Agent, version:'`
+START_MESSAGE=`kubectl logs dotnet-test-app | grep '- Elastic APM .NET Agent, version:'`
 if [ "x$START_MESSAGE" = "x" ]
 then
   exit 1
